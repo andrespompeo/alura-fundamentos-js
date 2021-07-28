8cf2c1b36fe2d1ffcc7442f88f7c330035be1594
Andre Pompeo <andrespompeo@gmail.com>
2021-07-28 08:33:16 -0300

    exemplos da aula 1

---
+++ type-string.js
@@ -0,0 +1,16 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+
+const citacao = "O Leo disse 'oi!'";
+
+// concatenação (+)
+const meuNome = "Andre"
+const citacao2 = "Meu nome é ";
+console.log(citacao2 + meuNome)
+
+
+
+
+
+
